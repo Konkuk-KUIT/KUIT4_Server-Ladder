fb73e12f6e05acf0df315920b5d134c8f46ea3b0
jyun27 <kimjyun27@gmail.com>
2024-09-17 10:23:18 +0900

    first commit

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -1,8 +1,48 @@
+import java.util.ArrayList;
+
 public class Ladder {
 
     private final int[][] rows;
+    private ArrayList<int[]> pairs = new ArrayList<>();
 
     public Ladder(int row, int numberOfPerson) {
         rows = new int[row][numberOfPerson];
+    }
+
+    public int validLadderIndex(int ladderIndex) {
+
+        if (ladderIndex < 0 || ladderIndex >= rows.length) {
+            System.out.println("Invalid ladder index");
+            return -1;
+        }
+
+        return ladderIndex;
+    }
+
+
+    public int run(int ladderIndex) {
+
+        int Pi = ladderIndex;
+        int Pj = 1;
+
+        for (int k = 1; k < rows.length; k++) {
+            Pj += 1;
+
+            for (int[] pair : pairs) {
+                if (pair[0] == Pi && pair[1] == Pj) {
+                    Pi += 1;
+                    continue;
+                }
+                if (pair[0] == Pi - 1 && pair[1] == Pj) {
+                    Pi -= 1;
+                }
+            }
+
+        }
+        return Pi;
+    }
+
+    public void drawLine(int i, int j) {
+        pairs.add(new int[]{i, j});
     }
 }
--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,5 +1,30 @@
+import org.junit.jupiter.api.Test;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
+
+    Scanner scanner = new Scanner(System.in);
+
+    @Test
+    void selectLadder2(){
+
+        Ladder ladder = new Ladder(4, 3);
+
+        int answer = ladder.run(ladder.validLadderIndex(5));
+        System.out.println(answer);
+    }
+
+    @Test
+    void 사다리_라인_생성(){
+
+        Ladder ladder = new Ladder(4, 3);
+
+        ladder.drawLine(2,3);
+        ladder.drawLine(1,4);
+        int i = ladder.run(ladder.validLadderIndex(3));
+        System.out.println(i);
+    }
 
 }
