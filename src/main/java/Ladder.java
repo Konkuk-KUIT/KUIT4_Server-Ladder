a460cfd36181a4b8e025d4c4520c163a21efed95
yskim6772 <yskim6772@gmail.com>
2024-09-19 00:04:03 +0900

    [chore] : fix the start value of index

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -7,14 +7,20 @@ public class Ladder {
     }
 
     public void drawLine(Position start, Position end) {
-        if (start.getRow() != end.getRow() || (start.getCol() != end.getCol() - 1 && start.getCol() != end.getCol() + 1)) {
+        int startRow = start.getRow() - 1;
+        int startCol = start.getCol() - 1;
+        int endRow = end.getRow() - 1;
+        int endCol = end.getCol() - 1;
+        if (startRow != endRow || (startCol != endCol - 1 && startCol != endCol + 1)) {
             throw new IllegalArgumentException("row가 다르거나, col이 1 차이가 아니어서 가로줄을 그릴 수 없음");
         }
 
-        rows[start.getRow()][Math.min(start.getCol(), end.getCol())] = 1;
+        rows[startRow][Math.min(startCol, endCol)] = 1;
     }
 
     public int run(int position) {
+        position = position - 1;
+
         for (int i = 0; i < rows.length; i++) {
             if (position > 0 && rows[i][position - 1] == 1) {
                 position--;
@@ -25,6 +31,6 @@ public class Ladder {
             }
         }
 
-        return position;
+        return position + 1;
     }
 }
--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LadderTest {
@@ -14,27 +13,27 @@ class LadderTest {
     @Test
     public void drawRows() {
         initColumns();
-        ladder.drawLine(new Position(0, 0), new Position(0, 1));
-        ladder.drawLine(new Position(0, 2), new Position(0, 3));
-        ladder.drawLine(new Position(2, 1), new Position(2, 2));
-        ladder.drawLine(new Position(1, 0), new Position(1, 1));
+        ladder.drawLine(new Position(1, 1), new Position(1, 2));
+        ladder.drawLine(new Position(1, 3), new Position(1, 4));
         ladder.drawLine(new Position(3, 2), new Position(3, 3));
+        ladder.drawLine(new Position(2, 1), new Position(2, 2));
+        ladder.drawLine(new Position(4, 3), new Position(4, 4));
     }
 
     @Test
     public void runLadder() {
         drawRows();
 
-        int result = ladder.run(0);
-        assertThat(result).isEqualTo(0);
-
-        result = ladder.run(1);
-        assertThat(result).isEqualTo(3);
+        int result = ladder.run(1);
+        assertThat(result).isEqualTo(1);
 
         result = ladder.run(2);
-        assertThat(result).isEqualTo(2);
+        assertThat(result).isEqualTo(4);
 
         result = ladder.run(3);
-        assertThat(result).isEqualTo(1);
+        assertThat(result).isEqualTo(3);
+
+        result = ladder.run(4);
+        assertThat(result).isEqualTo(2);
     }
 }
