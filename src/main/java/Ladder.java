78a086fe5a80f3ef5ed2d2b0faac938a9bd14c38
cyLee1111 <flora021111@gmail.com>
2024-09-18 23:32:50 +0900

    feat: add Ladder logic

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -5,4 +5,44 @@ public class Ladder {
     public Ladder(int row, int numberOfPerson) {
         rows = new int[row][numberOfPerson];
     }
+
+    public void drawLine(int x, int y) {
+        // check ladder index range
+        if (y + 1 >= rows[0].length || y < 0 || x >= rows.length || x < 0) {
+            throw new IllegalArgumentException("Line cannot be drawn: index is out of bounds.");
+        }
+
+        rows[x][y] = 1;
+        rows[x][y + 1] = -1;
+    }
+
+    public void printLadder() {
+        for (int i = 0; i < rows.length; i++) {
+            printRow(rows[i]);
+        }
+    }
+
+    private void printRow(int[] row) {
+        for (int j = 0; j < row.length; j++) {
+            System.out.print(row[j] + " ");
+        }
+        System.out.println();
+    }
+
+    public int run(int startLadder) {
+        int moveResult = startLadder;
+        for (int i = 0; i < rows.length; i++) {
+            switch (rows[i][moveResult]) {
+                case 0:
+                    break;
+                case 1:
+                    moveResult++;
+                    break;
+                case -1:
+                    moveResult--;
+                    break;
+            }
+        }
+        return moveResult;
+    }
 }
--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,5 +1,62 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
+
+    @Test
+    @DisplayName("사다리 라인 작성 확인")
+    void printLadder() {
+        Ladder ladder = new Ladder(4, 4);
+
+        ladder.drawLine(0,0);
+        ladder.drawLine(1,0);
+        ladder.drawLine(2,1);
+        ladder.drawLine(0,2);
+
+        ladder.printLadder();
+    }
+
+    @Test
+    @DisplayName("가장 바깥 사다리에는 라인을 그릴 수 없음")
+    void cannotDrawOutermost() {
+        Ladder ladder = new Ladder(4, 4);
+
+        assertThatThrownBy(() -> ladder.drawLine(1,3))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("사다리 범위 밖 접근")
+    void ladderOutOfBound() {
+        Ladder ladder = new Ladder(4, 4);
+
+        assertThatThrownBy(() -> ladder.drawLine(-1,4))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("사다리 타기 예시 (3->1)")
+    void runLadder() {
+        Ladder ladder = new Ladder(4, 4);
+
+        ladder.drawLine(0,0);
+        ladder.drawLine(1,0);
+        ladder.drawLine(2,1);
+        ladder.drawLine(0,2);
+
+        assertThat(ladder.run(3)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("사다리 타기 예시 (0->0)")
+    void runOneLineLadder() {
+        Ladder ladder = new Ladder(1, 1);
+
+        assertThat(ladder.run(0)).isEqualTo(0);
+    }
 
 }
