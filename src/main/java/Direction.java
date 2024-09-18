9c13f6602d2d4933bb630186f2541d6e2cfa4c8b
hd0rable <kanghuijin12@gmail.com>
2024-09-18 16:24:13 +0900

    refactor : direction -1,1 값에서 enum정의

---
+++ src/main/java/Direction.java
@@ -0,0 +1,13 @@
+public enum Direction {
+    LEFT(-1), RIGHT(1);
+
+    private final int value;
+
+    Direction(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
