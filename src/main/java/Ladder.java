31d955cfa62c4e941eddc51d406979699e1ae6d1
yskim6772 <yskim6772@gmail.com>
2024-09-18 21:30:12 +0900

    [feat] : implement Ladder, Position, LadderTest code

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -5,4 +5,26 @@ public class Ladder {
     public Ladder(int row, int numberOfPerson) {
         rows = new int[row][numberOfPerson];
     }
+
+    public void drawLine(Position start, Position end) {
+        if (start.getRow() != end.getRow() || (start.getCol() != end.getCol() - 1 && start.getCol() != end.getCol() + 1)) {
+            throw new IllegalArgumentException("row가 다르거나, col이 1 차이가 아니어서 가로줄을 그릴 수 없음");
+        }
+
+        rows[start.getRow()][Math.min(start.getCol(), end.getCol())] = 1;
+    }
+
+    public int run(int position) {
+        for (int i = 0; i < rows.length; i++) {
+            if (position > 0 && rows[i][position - 1] == 1) {
+                position--;
+                continue;
+            }
+            if (position < rows[0].length - 1 && rows[i][position] == 1) {
+                position++;
+            }
+        }
+
+        return position;
+    }
 }
--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,5 +1,40 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LadderTest {
 
+    private Ladder ladder;
+
+    @Test
+    public void initColumns() {
+        ladder = new Ladder(4, 4);
+    }
+
+    @Test
+    public void drawRows() {
+        initColumns();
+        ladder.drawLine(new Position(0, 0), new Position(0, 1));
+        ladder.drawLine(new Position(0, 2), new Position(0, 3));
+        ladder.drawLine(new Position(2, 1), new Position(2, 2));
+        ladder.drawLine(new Position(1, 0), new Position(1, 1));
+        ladder.drawLine(new Position(3, 2), new Position(3, 3));
+    }
+
+    @Test
+    public void runLadder() {
+        drawRows();
+
+        int result = ladder.run(0);
+        assertThat(result).isEqualTo(0);
+
+        result = ladder.run(1);
+        assertThat(result).isEqualTo(3);
+
+        result = ladder.run(2);
+        assertThat(result).isEqualTo(2);
+
+        result = ladder.run(3);
+        assertThat(result).isEqualTo(1);
+    }
 }
