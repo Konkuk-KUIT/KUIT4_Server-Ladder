6030ede100480522831d53490978e5ffe07104b9
hd0rable <kanghuijin12@gmail.com>
2024-09-18 16:24:55 +0900

    refactor : Position 캡슐화

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -3,7 +3,12 @@ public class Position {
     private int row;
     private int col;
 
-    public Position(int row, int col) {
+    public static Position createPosition(int row, int col) {
+       Position position =new Position(row,col);
+       return position;
+    }
+
+    private Position(int row, int col){
         if(row < 0 || col < 0){
             throw new IllegalArgumentException("행렬값은 음수값이 될 수 없습니다.");
         }
@@ -19,4 +24,7 @@ public class Position {
     {
         return col;
     }
+
+
+
 }
