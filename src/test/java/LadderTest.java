d938c1b3d448c7bda7443e8086d31ce9e9529d96
hd0rable <kanghuijin12@gmail.com>
2024-09-18 16:25:19 +0900

    test : 리팩토링에 따른 테스트 코드 수정 및 예외 테스트코드 추가

--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -10,15 +10,23 @@ class LadderTest {
     @Test
     @DisplayName("사다리생성")
     void CreateLadder() {
-        Ladder ladder = new Ladder(1,3);
+        Ladder ladder = Ladder.creatLadder(1,3);
         assertThat(ladder).isNotNull();
+    }
+
+    @Test
+    @DisplayName("사다리범위벗어난위치선택")
+    void CreateLadderLineExp(){
+        Ladder ladder = Ladder.creatLadder(4,4);
+        assertThatThrownBy(()->ladder.drawLine(Position.createPosition(1,5),Direction.RIGHT))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("사다리오른쪽라인생성")
     void CreateLadderLineRight(){
-        Ladder ladder = new Ladder(4,4);
-        ladder.drawLine(new Position(2,0),1);
+        Ladder ladder = Ladder.creatLadder(4,4);
+        ladder.drawLine(Position.createPosition(2,0),Direction.RIGHT);
 
         assertThat(ladder.getRows()[2][0]).isEqualTo(1);
         assertThat(ladder.getRows()[2][1]).isEqualTo(-1);
@@ -27,17 +35,17 @@ class LadderTest {
     @Test
     @DisplayName("사다리맨마지막오른쪽예외")
     void CreateLadderLineRightExp(){
-        Ladder ladder = new Ladder(4,4);
+        Ladder ladder = Ladder.creatLadder(4,4);
 
-        assertThatThrownBy(()->ladder.drawLine(new Position(1,3),1))
+        assertThatThrownBy(()->ladder.drawLine(Position.createPosition(1,3),Direction.RIGHT))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("사다리왼쪽라인생성")
     void CreateLadderLineLeft(){
-        Ladder ladder = new Ladder(4,4);
-        ladder.drawLine(new Position(1,2),-1);
+        Ladder ladder = Ladder.creatLadder(4,4);
+        ladder.drawLine(Position.createPosition(1,2),Direction.LEFT);
 
         assertThat(ladder.getRows()[1][2]).isEqualTo(-1);
         assertThat(ladder.getRows()[1][1]).isEqualTo(1);
@@ -46,21 +54,21 @@ class LadderTest {
     @Test
     @DisplayName("사다리맨처음왼쪽예외")
     void CreateLadderLineLeftExp(){
-        Ladder ladder = new Ladder(4,4);
+        Ladder ladder = Ladder.creatLadder(4,4);
 
-        assertThatThrownBy(()->ladder.drawLine(new Position(1,0),-1))
+        assertThatThrownBy(()->ladder.drawLine(Position.createPosition(1,0),Direction.LEFT))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("사다리이동")
     void LadderMove(){
-        Ladder ladder = new Ladder(4,4);
-        ladder.drawLine(new Position(0,0),1);
-        ladder.drawLine(new Position(0,2),1);
-        ladder.drawLine(new Position(1,0),1);
-        ladder.drawLine(new Position(2,1),1);
-        ladder.drawLine(new Position(3,2),1);
+        Ladder ladder = Ladder.creatLadder(4,4);
+        ladder.drawLine(Position.createPosition(0,0),Direction.RIGHT);
+        ladder.drawLine(Position.createPosition(0,2),Direction.RIGHT);
+        ladder.drawLine(Position.createPosition(1,0),Direction.RIGHT);
+        ladder.drawLine(Position.createPosition(2,1),Direction.RIGHT);
+        ladder.drawLine(Position.createPosition(3,2),Direction.RIGHT);
 
         assertThat( ladder.run(1)).isEqualTo(3);
 
@@ -69,13 +77,13 @@ class LadderTest {
     @Test
     @DisplayName("오른쪽연속적인사다리이동")
     void LadderMoveContinuousRight(){
-        Ladder ladder = new Ladder(4,4);
-        ladder.drawLine(new Position(0,0),1);
-        ladder.drawLine(new Position(0,1),1);
-        ladder.drawLine(new Position(0,2),1);
-        ladder.drawLine(new Position(1,0),1);
-        ladder.drawLine(new Position(2,1),1);
-        ladder.drawLine(new Position(3,2),1);
+        Ladder ladder = Ladder.creatLadder(4,4);
+        ladder.drawLine(Position.createPosition(0,0),Direction.RIGHT);
+        ladder.drawLine(Position.createPosition(0,1),Direction.RIGHT);
+        ladder.drawLine(Position.createPosition(0,2),Direction.RIGHT);
+        ladder.drawLine(Position.createPosition(1,0),Direction.RIGHT);
+        ladder.drawLine(Position.createPosition(2,1),Direction.RIGHT);
+        ladder.drawLine(Position.createPosition(3,2),Direction.RIGHT);
 
         assertThat( ladder.run(0)).isEqualTo(2);
 
@@ -84,19 +92,15 @@ class LadderTest {
     @Test
     @DisplayName("왼쪽연속적인사다리이동")
     void LadderMoveContinuousLeft(){
-        Ladder ladder = new Ladder(4,4);
-        ladder.drawLine(new Position(0,0),1);
-        ladder.drawLine(new Position(0,1),1);
-        ladder.drawLine(new Position(2,1),1);
-        ladder.drawLine(new Position(2,0),1);
+        Ladder ladder = Ladder.creatLadder(4,4);
+        ladder.drawLine(Position.createPosition(0,0),Direction.RIGHT);
+        ladder.drawLine(Position.createPosition(0,1),Direction.RIGHT);
+        ladder.drawLine(Position.createPosition(2,1),Direction.RIGHT);
+        ladder.drawLine(Position.createPosition(2,0),Direction.RIGHT);
 
         assertThat(ladder.run(0)).isEqualTo(0);
 
     }
 
-    //사다리왼쪽 라인생성//포지션 값예외터지는거//저기즉 ㅁ테스트에 값을 두개나확인하는데....그거좀 리팩토링
-    //칼럼이 0 일때 칼럼이 마지막일때 //글고 이제 이동구현// 커밋한다음에 //리팩토링하기
-    //리팩터링 캡슐화헤서 겟인스턴스함수로만들어서 생성자에서 바로못만들게 좀바꾸고 디렉션?도 클래스로
-    //만들어야할지도?
 
 }
