b1e2d4dc03b3d9383985fe55f8a2f1951694de47
yskim6772 <yskim6772@gmail.com>
2024-09-20 18:51:06 +0900

    [chore] : change test code example

--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -7,7 +7,7 @@ class LadderTest {
 
     @Test
     public void initColumns() {
-        ladder = new Ladder(4, 4);
+        ladder = new Ladder(5, 5);
     }
 
     @Test
@@ -15,9 +15,12 @@ class LadderTest {
         initColumns();
         ladder.drawLine(new Position(1, 1), new Position(1, 2));
         ladder.drawLine(new Position(1, 3), new Position(1, 4));
-        ladder.drawLine(new Position(3, 2), new Position(3, 3));
         ladder.drawLine(new Position(2, 1), new Position(2, 2));
+        ladder.drawLine(new Position(2, 4), new Position(2, 5));
+        ladder.drawLine(new Position(3, 2), new Position(3, 3));
         ladder.drawLine(new Position(4, 3), new Position(4, 4));
+        ladder.drawLine(new Position(5, 2), new Position(5, 3));
+        ladder.drawLine(new Position(5, 4), new Position(5, 5));
     }
 
     @Test
@@ -28,12 +31,15 @@ class LadderTest {
         assertThat(result).isEqualTo(1);
 
         result = ladder.run(2);
-        assertThat(result).isEqualTo(4);
+        assertThat(result).isEqualTo(5);
 
         result = ladder.run(3);
-        assertThat(result).isEqualTo(3);
+        assertThat(result).isEqualTo(4);
 
         result = ladder.run(4);
+        assertThat(result).isEqualTo(3);
+
+        result = ladder.run(5);
         assertThat(result).isEqualTo(2);
     }
 }
