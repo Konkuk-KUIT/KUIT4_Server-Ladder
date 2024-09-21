a3f6fa91691ff9454fd16b2561d71063b9226e1c
lylylylh <yh81260@naver.com>
2024-09-21 18:38:15 +0900

    기본 test

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -1,8 +1,37 @@
 public class Ladder {
 
-    private final int[][] rows;
+    private final int[][] positions;
 
-    public Ladder(int row, int numberOfPerson) {
-        rows = new int[row][numberOfPerson];
+    public Ladder(int height, int numberOfPerson) {
+        positions = new int[height][numberOfPerson];
+    }
+    public void drawLine(Position pos, int direction) {
+        if(direction == -1){
+            drawLineToLeft(pos);
+        }
+        if(direction == 1){
+            drawLineToRight(pos);
+        }
+    }
+    public void drawLineToLeft(Position pos) {
+        if(pos.getCol() <= 0 || pos.getCol() > (positions[0].length-1)){
+            throw new IllegalArgumentException("Index Error");
+        }
+        positions[pos.getRow()][pos.getCol()] = -1;
+        positions[pos.getRow()][pos.getCol()-1] = 1;
+    }
+    public void drawLineToRight(Position pos) {
+        if(pos.getCol() < 0 || pos.getCol() >= (positions[0].length-1)){
+            throw new IllegalArgumentException("Index Error");
+        }
+        positions[pos.getRow()][pos.getCol()] = 1;
+        positions[pos.getRow()][pos.getCol()+1] = -1;
+    }
+
+    public int run(int pos){
+        for (int i = 0; i < positions.length; i++) { // height
+            pos += positions[i][pos];
+        }
+        return pos;
     }
 }
