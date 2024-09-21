80246e434c6151f2092fc35b42b08354bd3c98e5
lylylylh <yh81260@naver.com>
2024-09-21 18:42:46 +0900

    Exception 추가

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -14,20 +14,31 @@ public class Ladder {
         }
     }
     public void drawLineToLeft(Position pos) {
-        if(pos.getCol() <= 0 || pos.getCol() > (positions[0].length-1)){
-            throw new IllegalArgumentException("Index Error");
-        }
+        LeftLinePossible(pos);
         positions[pos.getRow()][pos.getCol()] = -1;
         positions[pos.getRow()][pos.getCol()-1] = 1;
     }
     public void drawLineToRight(Position pos) {
-        if(pos.getCol() < 0 || pos.getCol() >= (positions[0].length-1)){
-            throw new IllegalArgumentException("Index Error");
-        }
+        RightLinePossible(pos);
         positions[pos.getRow()][pos.getCol()] = 1;
         positions[pos.getRow()][pos.getCol()+1] = -1;
     }
-
+    public void LeftLinePossible(Position pos) {
+        if(pos.getCol() <= 0 || pos.getCol() > (positions[0].length-1)){
+            throw new IllegalArgumentException("Index Error");
+        }
+        if(positions[pos.getRow()][pos.getCol()-1] != 0 || positions[pos.getRow()][pos.getCol()] != 0){
+            throw new IllegalArgumentException("Over Max Length of Line Error");
+        }
+    }
+    public void RightLinePossible(Position pos) {
+        if(pos.getCol() < 0 || pos.getCol() >= (positions[0].length-1)){
+            throw new IllegalArgumentException("Index Error");
+        }
+        if(positions[pos.getRow()][pos.getCol()+1] != 0 || positions[pos.getRow()][pos.getCol()] != 0){
+            throw new IllegalArgumentException("Over Max Length of Line Error");
+        }
+    }
     public int run(int pos){
         for (int i = 0; i < positions.length; i++) { // height
             pos += positions[i][pos];
--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -59,4 +59,34 @@ class LadderTest {
         int result = ladder.run(3); // 2
         assertThat(result).isEqualTo(0);  // 1
     }
+    @Test
+    void IndexExceptionCode(){
+        // => Test Fail : Index Error
+        int height = 5, numberOfPerson = 3;
+        Ladder ladder = new Ladder(height, numberOfPerson);
+        ladder.drawLine(new Position(0, 0), 1);
+        ladder.drawLine(new Position(1, 1), 1);
+        ladder.drawLine(new Position(2, 2), 1);
+
+        int result = ladder.run(0);
+        assertThat(result).isEqualTo(1);
+    }
+    @Test
+    void MaxLengthExceptionCode(){
+        // => Test Fail : Over Max Length of Line Error
+        int height = 5, numberOfPerson = 5;
+        Ladder ladder = new Ladder(height, numberOfPerson);
+        ladder.drawLine(new Position(0, 0), 1);
+        ladder.drawLine(new Position(1, 1), 1);
+        ladder.drawLine(new Position(2, 1), -1);
+        ladder.drawLine(new Position(2, 2), -1);
+        ladder.drawLine(new Position(2, 3), -1);
+        ladder.drawLine(new Position(3, 1), 1);
+        ladder.drawLine(new Position(3, 4), -1);
+        ladder.drawLine(new Position(4, 2), 1);
+        ladder.drawLine(new Position(4, 0), 1);
+
+        int result = ladder.run(3); // 2
+        assertThat(result).isEqualTo(0);  // 1
+    }
 }
