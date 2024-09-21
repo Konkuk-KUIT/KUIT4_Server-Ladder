6acb16c66bce9c5667f13c5dcee74366ac461f68
lylylylh <yh81260@naver.com>
2024-09-21 17:22:40 +0900

    기본 test

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -1,22 +1,36 @@
 public class Ladder {
-    // 따로 verify 함수 생성
 
-    private final Row[] rows;
+    private final int[][] positions;
 
-    public Ladder(int row, int ladderNum) {
-        rows = new Row[row];
-        for (int i = 0; i < row; i++) {
-            rows[i] = new Row(ladderNum);
+    public Ladder(int height, int numberOfPerson) {
+        positions = new int[height][numberOfPerson];
+    }
+    public void drawLine(Position pos, int direction) {
+        if(direction == -1){
+            drawLineToLeft(pos);
+        }
+        if(direction == 1){
+            drawLineToRight(pos);
         }
     }
-    public void drawLine(int row, int col) {
-        rows[row].drawLine(col);
+    public void drawLineToLeft(Position pos) {
+        if(pos.getCol() <= 0 || pos.getCol() > (positions[0].length-1)){
+            throw new IllegalArgumentException("Index Error");
+        }
+        positions[pos.getRow()][pos.getCol()] = -1;
+        positions[pos.getRow()][pos.getCol()-1] = 1;
+    }
+    private void drawLineToRight(Position pos) {
+        if(pos.getCol() < 0 || pos.getCol() >= (positions[0].length-1)){
+            throw new IllegalArgumentException("Index Error");
+        }
+        positions[pos.getRow()][pos.getCol()] = 1;
+        positions[pos.getRow()][pos.getCol()+1] = -1;
     }
 
     public int run(int pos){
-        // position에 따라 계속 movePos 옮기면서 row가 사다리 높이가 될때까지
-        for (int i = 0; i < rows.length; i++) {
-            pos = rows[i].movePos(pos);
+        for (int i = 0; i < positions.length; i++) { // height
+            pos += positions[i][pos];
         }
         return pos;
     }
--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,25 +1,63 @@
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
+    Position pos;
+
     @Test
     void Ladder_init() {
-        int row = 5, col = 5;
-
-        Ladder ladder = new Ladder(row, col);
-
+        int height = 5, numberOfPerson = 5;
+        Ladder ladder = new Ladder(height, numberOfPerson);
         assertThat(ladder).isNotNull();
+    }
+    @Test
+    void OnePerson(){
+        int height = 5, numberOfPerson = 1;
+        Ladder ladder = new Ladder(height, numberOfPerson);
+
+        int result = ladder.run(0);
+        assertThat(result).isEqualTo(0);
+    }
+    @Test
+    void TwoPerson(){
+        int height = 5, numberOfPerson = 2;
+        Ladder ladder = new Ladder(height, numberOfPerson);
+        ladder.drawLine(new Position(0, 0), 1);
+        ladder.drawLine(new Position(2, 1), -1);
+        ladder.drawLine(new Position(3, 1), -1);
+
+        int result = ladder.run(0);
+        assertThat(result).isEqualTo(1);
     }
 
     @Test
-    void Ladder() {
+    void ThreePerson(){
+        int height = 5, numberOfPerson = 3;
+        Ladder ladder = new Ladder(height, numberOfPerson);
+        ladder.drawLine(new Position(0, 0), 1);
+        ladder.drawLine(new Position(1, 1), 1);
+        ladder.drawLine(new Position(2, 2), -1);
+        //ladder.drawLine(new Position(3, 1), -1); => 0
 
-        Ladder ladder = new Ladder(3, 4);
-        ladder.drawLine(0, 1);
-        ladder.drawLine(1, 2);
+        int result = ladder.run(0);
+        assertThat(result).isEqualTo(1);
+    }
+    @Test
+    void FivePerson(){
+        int height = 5, numberOfPerson = 5;
+        Ladder ladder = new Ladder(height, numberOfPerson);
+        ladder.drawLine(new Position(0, 0), 1);
+        ladder.drawLine(new Position(1, 1), 1);
+        ladder.drawLine(new Position(2, 1), -1);
+        ladder.drawLine(new Position(2, 3), -1);
+        ladder.drawLine(new Position(3, 1), 1);
+        ladder.drawLine(new Position(3, 4), -1);
+        ladder.drawLine(new Position(4, 2), 1);
+        ladder.drawLine(new Position(4, 0), 1);
 
-        int result = ladder.run(1);
-        assertThat(result).isEqualTo(2);
+        int result = ladder.run(3); // 2
+        assertThat(result).isEqualTo(0);  // 1
     }
 }
