88164f690adc061cedd6cf920648d71f24ae2551
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-09-22 22:23:34 +0900

    comment: 리팩 사항 정리

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -4,6 +4,7 @@ public class Ladder {
 
     private final Row[] rows;
 
+//    todo 자연수 래퍼 클래스
     public Ladder(int numberOfRow, int numberOfPerson) {
         rows = new Row[numberOfRow];
         for (int i = 0; i < numberOfRow; i++) {
@@ -12,12 +13,15 @@ public class Ladder {
     }
 
 //    사다리 가로줄을 긋는 로직
+//    todo LadderCreator 분리
     public void drawLine(int row, int col) {
         rows[row].drawLine(col);
     }
 
 //    사다리에 position값을 넣어주고 반복문을 통해
 //    메세지를 보내면서 다음 좌표가 어딨는지 질문을 함
+//    todo 포지션 래퍼 클래스
+//    todo LadderRunner 분리
     public int run(int position) {
         for (int i = 0; i < rows.length; i++) {
             position = rows[i].nextPosition(position);
--- src/main/java/ladder/Row.java
+++ src/main/java/ladder/Row.java
@@ -1,6 +1,8 @@
 package ladder;
 
 public class Row {
+
+//    todo Node 클래스 분리
     private final int[] row;
 
     public Row(int numberOfPerson) {
@@ -10,6 +12,8 @@ public class Row {
 
 //    사다리 가로줄을 그을 때 해당 좌표에는 1을 넣고 오른쪽 값은 -1을 해줘서
 //    입력한 좌표의 오른쪽에 라인이 생기게끔 만듦
+//    이런식으로 상수가 하드코딩이 돼있는건 좋지 않음
+//    todo 상수 리팩토링 0, -1, 1
     public void drawLine(int startPosition) {
         validateDrawLinePosition(startPosition);
         row[startPosition] = 1;
