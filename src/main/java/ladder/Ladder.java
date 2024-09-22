cb14c2ded66c5013b1867f050ad40e11dadc583e
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-09-22 22:19:52 +0900

    comment: 코드 설명 주석 추가

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -11,10 +11,13 @@ public class Ladder {
         }
     }
 
+//    사다리 가로줄을 긋는 로직
     public void drawLine(int row, int col) {
         rows[row].drawLine(col);
     }
 
+//    사다리에 position값을 넣어주고 반복문을 통해
+//    메세지를 보내면서 다음 좌표가 어딨는지 질문을 함
     public int run(int position) {
         for (int i = 0; i < rows.length; i++) {
             position = rows[i].nextPosition(position);
--- src/main/java/ladder/Row.java
+++ src/main/java/ladder/Row.java
@@ -8,12 +8,15 @@ public class Row {
         row = new int[numberOfPerson];
     }
 
+//    사다리 가로줄을 그을 때 해당 좌표에는 1을 넣고 오른쪽 값은 -1을 해줘서
+//    입력한 좌표의 오른쪽에 라인이 생기게끔 만듦
     public void drawLine(int startPosition) {
         validateDrawLinePosition(startPosition);
         row[startPosition] = 1;
         row[startPosition + 1] = -1;
     }
 
+//    자신의 상태가 무엇인지에 따라서 다음 좌표를 계산하고 반환을 함
     public int nextPosition(int position) {
         validatePosition(position);
 
