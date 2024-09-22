1d6b238e36e884161ee593e05189bda639621f0a
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-09-22 22:32:37 +0900

    refactor: 상수 없애기 리팩 완료

--- src/main/java/ladder/Row.java
+++ src/main/java/ladder/Row.java
@@ -1,9 +1,12 @@
 package ladder;
 
+import static ladder.Direction.*;
+
 public class Row {
 
 //    todo Node 클래스 분리
     private final int[] row;
+
 
     public Row(int numberOfPerson) {
         validateNumberOfPerson(numberOfPerson);
@@ -13,11 +16,10 @@ public class Row {
 //    사다리 가로줄을 그을 때 해당 좌표에는 1을 넣고 오른쪽 값은 -1을 해줘서
 //    입력한 좌표의 오른쪽에 라인이 생기게끔 만듦
 //    이런식으로 상수가 하드코딩이 돼있는건 좋지 않음
-//    todo 상수 리팩토링 0, -1, 1
     public void drawLine(int startPosition) {
         validateDrawLinePosition(startPosition);
-        row[startPosition] = 1;
-        row[startPosition + 1] = -1;
+        row[startPosition] = RIGHT.getValue();
+        row[startPosition + 1] = LEFT.getValue();
     }
 
 //    자신의 상태가 무엇인지에 따라서 다음 좌표를 계산하고 반환을 함
@@ -34,11 +36,11 @@ public class Row {
     }
 
     private boolean isLeft(int position) {
-        return row[position] == -1;
+        return row[position] == LEFT.getValue();
     }
 
     private boolean isRight(int position) {
-        return row[position] == 1;
+        return row[position] == RIGHT.getValue();
     }
 
     private void validateNumberOfPerson(int numberOfPerson) {
