019db056d3bbadaaf86b29851f69de87f0e86ee1
ksg <ksg001227@naver.com>
2024-09-23 22:17:31 +0900

    test: 사다리 출력 기능 테스트

--- src/test/java/ladder/LadderPrintTest.java
+++ src/test/java/ladder/LadderPrintTest.java
@@ -5,6 +5,22 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class LadderPrintTest {
+    @Test
+    void 사다리_출력_테스트() {
+        // given
+        GreaterThanOne numberOfPerson = GreaterThanOne.from(5);
+        GreaterThanOne row = GreaterThanOne.from(5);
+        LadderCreator ladderCreator = new LadderCreator(row, numberOfPerson);
+        LadderGame ladderGame = new LadderGame(ladderCreator);
 
+        ladderCreator.drawLine(Position.from(0), Position.from(0));
+        ladderCreator.drawLine(Position.from(1), Position.from(1));
+
+        // when
+        Position position = Position.from(0);
+
+        // then
+        Assertions.assertThat(ladderGame.run(position)).isEqualTo(2);
+    }
 
 }
