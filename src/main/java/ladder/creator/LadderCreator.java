dce5bbefc634a64de089ae2ea1750aad488164b5
ksg <ksg001227@naver.com>
2024-09-23 23:50:10 +0900

    feat: 사다리 자동 생성 기능 추가

--- src/main/java/ladder/creator/LadderCreator.java
+++ src/main/java/ladder/creator/LadderCreator.java
@@ -4,6 +4,8 @@ import ladder.GreaterThanOne;
 import ladder.Position;
 import ladder.Row;
 
+import java.util.Random;
+
 public class LadderCreator {
 
     private final Row[] rows;
@@ -17,6 +19,37 @@ public class LadderCreator {
 
     public void drawLine(Position row, Position col) {
         rows[row.getValue()].drawLine(col);
+    }
+
+
+    // 사다리 자동 생성
+    public void autoDrawLine() {
+        int numberOfLines = (int) (rows.length * rows[0].getNodes().length * 0.3);
+
+        while (numberOfLines > 0) {
+
+            Position row = selectRandomRow();
+            Position col = selectRandomCol();
+
+            try {
+                rows[row.getValue()].drawLine(col);
+            } catch (IllegalArgumentException e) {  //예외 처리 로직은 이미 존재하니, 예외가 발생하면 다시 사다리 그리기
+                continue;
+            }
+
+            numberOfLines--;
+        }
+
+    }
+
+    private Position selectRandomRow() {
+        Random rand = new Random();
+        return Position.from(rand.nextInt(rows.length - 1));
+    }
+
+    private Position selectRandomCol() {
+        Random rand = new Random();
+        return Position.from(rand.nextInt((rows[0].getNodes().length - 1)));
     }
 
     public Row[] getRows() {
---
+++ src/test/java/ladder/LadderAutoCreateTest.java
@@ -0,0 +1,8 @@
+package ladder;
+
+import ladder.creator.LadderCreator;
+import org.junit.jupiter.api.Test;
+
+public class LadderAutoCreateTest {
+
+}
