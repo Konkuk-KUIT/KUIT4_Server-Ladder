b9162ed40e86ca8efaca1bf7001f92775b36de2f
hd0rable <kanghuijin12@gmail.com>
2024-09-23 16:08:53 +0900

    feat : 기능 요구사항 2 사다리 출력 기능 추가

--- src/main/java/ladder/LadderRunner.java
+++ src/main/java/ladder/LadderRunner.java
@@ -8,9 +8,48 @@ public class LadderRunner {
         this.rows = rows;
     }
 
+    public void drawLadderState(Position position,int rowcnt) {
+
+        StringBuilder line = new StringBuilder();
+        for (int i = 0; i < rows.length; i++) {
+            Node[] nodes = rows[i].getNodes();
+            //LadderStateUpdate(line,position,nodes);
+            for(int j = 0; j < nodes.length; j++) {
+                if (nodes[j].isRight())
+                    line.append(1);
+                if (nodes[j].isLeft())
+                    line.append(-1);
+                if (nodes[j].isNone())
+                    line.append(0);
+                if(position.getValue() == j && rowcnt==i)
+                    line.append("*");
+                line.append(" ");
+            }
+            line.append("\n");
+        }
+        System.out.println(line.toString());
+    }
+
+//    public void LadderStateUpdate(StringBuilder line,Position position,Node[] nodes) {
+//        for(int j = 0; j < nodes.length; j++) {
+//            if (nodes[j].isRight())
+//                line.append(1);
+//            if (nodes[j].isLeft())
+//                line.append(-1);
+//            if (nodes[j].isNone())
+//                line.append(0);
+//            if(position.getValue() == j)
+//                line.append("*");
+//            line.append(" ");
+//        }
+//
+//    }
+
     public int run(Position position) {
         for (int i = 0; i < rows.length; i++) {
+            drawLadderState(position,i);
             rows[i].nextPosition(position);
+            drawLadderState(position,i);
         }
         return position.getValue();
     }
