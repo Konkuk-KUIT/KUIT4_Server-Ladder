8e6897b79a875c659e151456242a01e50162f81a
seungwoo1209 <2002msw@naver.com>
2024-09-24 17:13:09 +0900

    deleted unnecessary class: gameEntry

--- src/main/java/ladder/Position.java
+++ src/main/java/ladder/Position.java
@@ -1,24 +1,24 @@
 package ladder;
 
 public class Position {
-    private gameEntry entry;
+    private unsignedInteger row;
+    private unsignedInteger col;
     private Direction direction;
 
-    private Position(gameEntry entry, Direction direction) {
-        this.entry = entry;
+    private Position(int row, int col, Direction direction) {
+        this.row = unsignedInteger.from(row);
+        this.col = unsignedInteger.from(col);
         this.direction = direction;
     }
 
     public static Position of(int row, int column, Direction direction) {
-        return new Position(gameEntry.from(row,column), direction);
+        return new Position(row ,column ,direction);
     }
 
-    public int getRow() {
-        return this.entry.getRow();
-    }
+    public int getRow() { return this.row.getValue(); }
 
     public int getColumn() {
-        return this.entry.getColumn();
+        return this.col.getValue();
     }
 
     public Direction getDirection() {
