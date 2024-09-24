0ba770718b414ca2af14f75c60b79328139e5663
ksg <ksg001227@naver.com>
2024-09-24 15:31:21 +0900

    refactor: LadderPosition 클래스 생성

---
+++ src/main/java/ladder/LadderPosition.java
@@ -0,0 +1,32 @@
+package ladder;
+
+public class LadderPosition {
+
+    private Position row;
+    private Position col;
+
+    private LadderPosition(Position row, Position col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public static LadderPosition from(Position row, Position col) {
+        return new LadderPosition(row, col);
+    }
+
+    public Position getRow() {
+        return row;
+    }
+
+    public Position getCol() {
+        return col;
+    }
+
+    public boolean isCurrentRow(Position row) {
+        return this.row.getValue() == row.getValue();
+    }
+
+    public boolean isCurrentCol(Position col) {
+        return this.col.getValue() == col.getValue();
+    }
+}
